dd5fd176b6b44853888ae4cd313cb8920c466791
Nikola Kelava <nikola.kelava94@gmail.com>
2022-01-24 22:21:45 +0100

    Start to add redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,30 +1,56 @@
-import React, { useState, useEffect, useReducer } from 'react';
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
+import './App.css';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll'
 import ErrorBoundry from '../components/ErrorBoundry';
-import './App.css';
+import { setSearchField, requestRobots } from '../actions';
  
 
+const mapStateToProps = (state) => {
+    console.log("map state to props");
+    return {
+        searchField:    state.searchRobots.searchField,
+        robots:         state.requestRobots.robots,
+        isPending:      state.requestRobots.isPending,
+        error:          state.requestRobots.error
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    console.log("map dispatch to props");
+    return {
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+        onRequestRobots: () => dispatch(requestRobots())
+    }
+}
+
 function App() {
-    const [robots, setRobots] = useState([]);
-    const [searchField, setSearchField] = useState("");
+    const { searchField, onSearchChange } = this.props;
+    const { robots, isPending } =  this.props;
+
+    // console.log(robots);
 
     useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/users')
-        .then(response => response.json())
-        .then(users => setRobots(users));
+        // fetch('https://jsonplaceholder.typicode.com/users')
+        // .then(response => response.json())
+        // .then(users => setRobots(users));
+        this.props.onRequestRobots();
     }, []);
 
-    const onSearchChange = (event) => {
-        setSearchField(event.target.value);
-    }
+    // const onSearchChange = (event) => {
+    //     setSearchField(event.target.value);
+    // }
 
     const filteredRobots = robots.filter(robot => {
         return robot.name.toLowerCase().includes(searchField.toLowerCase());
     });
 
-    if (!robots.length) {
+    // if (!robots.length) {
+    //     return <h1>Loading...</h1>
+    // }
+    if (!isPending) {
         return <h1>Loading...</h1>
     }
     
@@ -41,4 +67,4 @@ function App() {
     );
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 import App from './containers/App';
 import 'tachyons';
+import { searchRobots, requestRobots } from './reducers';
 
+const logger = createLogger();
+
+const rootReducers = combineReducers({ requestRobots, searchRobots });
+const store = createStore(rootReducers, applyMiddleware(thunkMiddleware, logger));
 
 ReactDOM.render(
     <React.StrictMode>
-        <App/>
+        <Provider store={store}>
+            <App/>
+        </Provider>
     </React.StrictMode>
     , document.getElementById("root"));
 
